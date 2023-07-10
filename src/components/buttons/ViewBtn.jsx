52941bb369abad26b07c06f464a079d6405c3938
ScottDeev <amehelijah99@gmail.com>
2023-07-10 15:09:00 +0100

    added the fonts

--- src/components/buttons/ViewBtn.jsx
+++ src/components/buttons/ViewBtn.jsx
@@ -3,10 +3,10 @@ import arrow from '../../assets/arrow.svg'
 import './button.css'
 export default function ViewBtn() {
   return (
-    <div className='viewBtn hover:text-primary text-accent4 flex items-center gap-[5px] transition duration-300 ease-in-out cursor-pointer'>
-      <span className=''>View More</span>
+    <button className='viewBtn hover:text-primary text-accent4 flex items-center gap-[5px] transition duration-300 ease-in-out cursor-pointer'>
+      <span className='font-[Goemetric-415-Black-BT]'>View More</span>
       <img className='iconlight' src={arrow} alt="info" />
       <img className='icon' src={arrowW} alt="info" />
-    </div>
+    </button>
   )
 }
