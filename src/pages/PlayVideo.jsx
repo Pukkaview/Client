746b219f1c87cb15b8bc63a66a3142b7284f4a9a
ScottDeev <amehelijah99@gmail.com>
2023-07-25 18:50:51 +0100

    test

--- src/pages/PlayVideo.jsx
+++ src/pages/PlayVideo.jsx
@@ -1,4 +1,29 @@
+import { useEffect } from 'react';
+import ReactPlayer from 'react-player'
+import Fetcher from '../utils/fetcher';
+
 const PlayVideo = () => {
-  return <div>PlayVideo</div>;
+  useEffect(() => {
+    // Function to fetch the video URL
+    const fetchVideoUrl = async () => {
+      try {
+        const fetchResponse = await Fetcher("https://pukkaview-backend.onrender.com/videoplayer/1/play/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "video/mp4",
+          },
+        });
+        console.log(fetchResponse);
+      } catch (error) {
+        console.error('Error fetching video URL:', error);
+      }
+    };
+
+    fetchVideoUrl();
+  }, []);
+  return <div>
+    <ReactPlayer url='https://pukkaview-backend.onrender.com/videoplayer/1/play/' />
+    <video src="https://youtu.be/pWxDxhWXJos"></video>
+  </div>;
 };
 export default PlayVideo;
