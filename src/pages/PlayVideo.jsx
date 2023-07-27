9ce97b50c3dc3e03bd3748bce77353033a721d23
ScottDeev <amehelijah99@gmail.com>
2023-07-27 21:06:39 +0100

    testing

--- src/pages/PlayVideo.jsx
+++ src/pages/PlayVideo.jsx
@@ -1,29 +1,29 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import ReactPlayer from 'react-player'
-import Fetcher from '../utils/fetcher';
+// import Fetcher from '../utils/fetcher';
 
 const PlayVideo = () => {
-  useEffect(() => {
-    // Function to fetch the video URL
-    const fetchVideoUrl = async () => {
-      try {
-        const fetchResponse = await Fetcher("https://pukkaview-backend.onrender.com/videoplayer/1/play/", {
-          method: "GET",
-          headers: {
-            "Content-Type": "video/mp4",
-          },
-        });
-        console.log(fetchResponse);
-      } catch (error) {
-        console.error('Error fetching video URL:', error);
-      }
-    };
+  // useEffect(() => {
+  //   // Function to fetch the video URL
+  //   const fetchVideoUrl = async () => {
+  //     try {
+  //       const fetchResponse = await Fetcher("http://34.205.31.29:8000/videoplayer/1/play/", {
+  //         method: "GET",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       });
+  //       console.log(fetchResponse);
+  //     } catch (error) {
+  //       console.error('Error fetching video URL:', error);
+  //     }
+  //   };
 
-    fetchVideoUrl();
-  }, []);
+  //   fetchVideoUrl();
+  // }, []);
   return <div>
-    <ReactPlayer url='https://pukkaview-backend.onrender.com/videoplayer/1/play/' />
-    <video src="https://youtu.be/pWxDxhWXJos"></video>
+    <ReactPlayer url='https://admin.pukkaview.com/dashboard/Videos/Boruto%20Episode%20197.mp4' />
+    <video src="http://34.205.31.29:8000/videoplayer/1/play/"></video>
   </div>;
 };
 export default PlayVideo;
