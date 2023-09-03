86317fb76e4647f9a146372f09b73521fc4d0054
ScottDeev <amehelijah99@gmai.com>
2023-09-03 23:48:07 +0100

    updates

--- src/components/buttons/WatchBtn.jsx
+++ src/components/buttons/WatchBtn.jsx
@@ -1,9 +1,9 @@
 import play from '../../assets/playwhite.svg'
 export default function WatchBtn() {
   return (
-    <button className='px-[18px] sm:px-[20px] sm:py-[18px] py-[14px] border-[1px] border-accent4 bg-primary hover:bg-accent2 transition duration-300 ease-in-out cursor-pointer flex sm:gap-[19px] gap-[10px] justify-center rounded-[10px]'>
-    <img className='sm:flex hidden' src={play} alt="play" />
-    <span className='text-text-color sm:text-[16px] text-[14px] font-[Goemetric-415-Black-BT]'>Watch now</span>
+    <button className='px-[18px] sm:px-[20px] sm:py-[14px] py-[14px] border-[1px] border-accent4 bg-primary hover:bg-accent2 transition duration-300 ease-in-out cursor-pointer flex sm:gap-[19px] gap-[10px] justify-center items-center rounded-[10px]'>
+    <img className='sm:flex hidden h-[14px]' src={play} alt="play" />
+    <span className='text-text-color sm:text-[14px] text-[12px] font-[Goemetric-415-Black-BT]'>Watch now</span>
     </button>
   )
 }
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -58,7 +58,7 @@ const Categories = () => {
             <Skeleton animation="wave" variant="rectangular"  height={199} sx={{ backgroundColor: 'rgba(255, 255, 255, 0.1)', color: 'white', borderRadius: '15px' }} />
             <Skeleton animation="wave" variant="rectangular"  height={199} sx={{ backgroundColor: 'rgba(255, 255, 255, 0.1)', color: 'white', borderRadius: '15px' }} />
               </div>}
-              {data.length > 0 && <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-4 gap-2 pt-10 mx-auto">
+              {data.length > 0 && <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-4 gap-2 pt-5 mx-auto">
               {data.map((c,i)=> (
                 <div className={`flex sm:mb-[20px] ${(i + 4)%4 === 0 ? 'lg:justify-start': (i + 1)%4 === 0 ? 'lg:justify-end': 'lg:justify-center'}`}>
                   <MovieCard2 key={c.id} data={c} playIcon={i < 2 ? true:false} />
