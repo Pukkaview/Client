1b61760a7d1f7bd33734d5fb1891bd56812cfbbd
ScottDeev <amehelijah99@gmai.com>
2023-11-26 12:06:16 +0100

    update

--- src/components/player/customVideoPlayer.jsx
+++ src/components/player/customVideoPlayer.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-
+import logo from '../../assets/logo.svg'
 
 // eslint-disable-next-line react/prop-types
 const CustomVideoPlayer = ({ data }) => {
   const [loading, setLoading] = useState(true)
-  console.log(loading);
-  console.log(data);
   const handleLoad = () => {
     setLoading(false)
   }
@@ -15,9 +13,13 @@ const CustomVideoPlayer = ({ data }) => {
     className={`video_container w-full fixed sm:relative top-[70px] z-[30] flex justify-center flex-col bg-[#000] `} 
     >
         {loading && (
+          <>
         <div className="absolute md:top-0 sm:top-[40px] left-0 w-full h-full flex justify-center items-center">
           <img src={logo} alt="Zoom In Image" className="zoom-in-out-animation" />
         </div>
+        {!data.video_id && <div className='sm:h-[400px] h-[200px]'></div>}
+          </>
+        
       )}
           {data.video_id && <iframe className='sm:h-[400px] h-[200px]' src={`https://iframe.mediadelivery.net/embed/${data.video_library_id}/${data.video_id}?autoplay=true&loop=false&muted=false&preload=true`} loading="lazy" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowfullscreen="true" onLoad={handleLoad}></iframe>}
     </div>
